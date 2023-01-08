08eb07f4c91712b8508ba92b6412c7fc064cf6d3
sguha-work <sguha1988.life@gmail.com>
2023-01-08 11:11:49 +0530

    changes

---
+++ backend/modules/users/app.js
@@ -0,0 +1,49 @@
+//const serverless = require("serverless-http");
+import express from 'express';
+import bodyParser from 'body-parser';
+import fs from 'fs';
+import https from 'https';
+import UserController from "./controllers/user_controller.js";
+import ErrorHandler from './controllers/error_controller.js';
+const eh = ErrorHandler.getInstance();
+const app = express();
+app.use(bodyParser.json());
+
+const key = fs.readFileSync('./key.pem');
+const cert = fs.readFileSync('./cert.pem');
+const server = https.createServer({key: key, cert: cert }, app);
+server.listen(3005, () => {
+    console.log(`App is running on port 3005`);
+});
+const userController = UserController.instance;
+app.get("/users", async (request, response, next) => {
+  try {
+    const users = await userController.get(request.query);
+    return response.status(200).json({
+      data: users,
+    });
+  } catch (error) {
+    response.send(eh.handle(error));
+  }
+});
+app.post("/users", async (request, response) => {// create, create new teacher
+  let responseObj = {};
+  responseObj.status = 201;
+  responseObj.data = {};
+  responseObj.message = '';
+  try {
+    console.log(request.body);
+    const result = await userController.create(request.body);
+    responseObj.data = result;
+    response.send(responseObj);
+  } catch (error) {
+    response.send(eh.handle(error));
+  }
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
